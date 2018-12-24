143234c41baf3722641f52cb3bfd91289e99bcc7
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2018-12-24 15:40:41 +0100

    Switch: tomorrow

--- weekday-plugin-script.js
+++ weekday-plugin-script.js
@@ -6,30 +6,37 @@ switch (weekday) {
         
     case 0: 
         dayZero = "Sonntag";
+        dayOne = "Montag";
         break;
     
     case 1: 
         dayZero = "Montag";
+        dayOne = "Dienstag";
         break;
     
     case 2: 
         dayZero = "Dienstag";
+        dayOne = "Mittwoch";
         break;
         
     case 3: 
         dayZero = "Mittwoch";
+        dayOne = "Donnerstag";
         break;
         
     case 4: 
         dayZero = "Donnerstag";
+        dayOne = "Freitag";
         break;
         
     case 5: 
         dayZero = "Freitag";
+        dayOne = "Samstag";
         break;
         
     case 6: 
         dayZero = "Samstag";
+        dayOne = "Sonntag";
         break;
         
 }
