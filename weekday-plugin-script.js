a184b81b90df2e917146e7e73e9364b84b4299fb
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2018-12-24 15:53:07 +0100

    Switch: in three days

--- weekday-plugin-script.js
+++ weekday-plugin-script.js
@@ -8,42 +8,49 @@ switch (weekday) {
         dayZero = "Sonntag";
         dayOne = "Montag";
         dayTwo = "Dienstag";
+        dayThree = "Mittwoch";
         break;
     
     case 1: 
         dayZero = "Montag";
         dayOne = "Dienstag";
         dayTwo = "Mittwoch";
+        dayThree = "Donnerstag";
         break;
     
     case 2: 
         dayZero = "Dienstag";
         dayOne = "Mittwoch";
         dayTwo = "Donnerstag";
+        dayThree = "Freitag";
         break;
         
     case 3: 
         dayZero = "Mittwoch";
         dayOne = "Donnerstag";
         dayTwo = "Freitag";
+        dayThree = "Samstag";
         break;
         
     case 4: 
         dayZero = "Donnerstag";
         dayOne = "Freitag";
         dayTwo = "Samstag";
+        dayThree = "Sonntag";
         break;
         
     case 5: 
         dayZero = "Freitag";
         dayOne = "Samstag";
         dayTwo = "Sonntag";
+        dayThree = "Montag";
         break;
         
     case 6: 
         dayZero = "Samstag";
         dayOne = "Sonntag";
         dayTwo = "Montag";
+        dayThree = "Dienstag";
         break;
         
 }
